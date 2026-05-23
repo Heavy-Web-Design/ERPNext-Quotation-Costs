751b8ba273c712ed6ea48644f49cd2c1bd75d9f5
Juan Pablo Lopez Garciaguirre <juanpablo@heavywebdesign.com>
2026-05-23 04:13:08 +0000

    Quotation Costs Items: Define fields label and position

--- quotation_costs/setup/custom_fields.py
+++ quotation_costs/setup/custom_fields.py
@@ -13,16 +13,15 @@ def add_custom_fields():
     # Add custom fields
     custom_fields = {
         "Quotation": [
-            dict(fieldname='section_quote_costs_items', label='Quotation Costs', fieldtype='Section Break', insert_after='net_total', hide_border=True),
+            dict(fieldname='section_quote_costs_items', label='Quotation Costs', fieldtype='Section Break', insert_after='net_total', hide_border=True, collapsible=True),
             dict(fieldname='quote_costs_items', label='', fieldtype='Table', options='Quotation Costs Item', insert_after='section_quote_costs_items'),
             dict(fieldname='section_quote_costs_totals', label='', fieldtype='Section Break', insert_after='quote_costs_items'),
             dict(fieldname='column_break_qc1', label='', fieldtype='Column Break', insert_after='section_quote_costs_totals'),
             dict(fieldname='column_break_qc2', label='', fieldtype='Column Break', insert_after='column_break_qc1'),
             dict(fieldname='column_break_qc3', label='', fieldtype='Column Break', insert_after='column_break_qc2'),
-            dict(fieldname='total_average_profit_percent', label='Total Average Profit Percent', fieldtype='Float', insert_after='column_break_qc3'),
+            dict(fieldname='total_average_profit_percent', label='Total Average Profit (%)', fieldtype='Float', insert_after='column_break_qc3'),
             dict(fieldname='column_break_qc4', label='', fieldtype='Column Break', insert_after='total_average_profit_percent'),
-            dict(fieldname='total_profit_amount', label='Total Profit Amount', fieldtype='Currency', insert_after='column_break_qc4'),
-
+            dict(fieldname='total_profit_amount', label='Total Profit', fieldtype='Currency', insert_after='column_break_qc4'),
         ]
     }
 
